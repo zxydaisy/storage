48dc2bac7da65aae0a922868bb3b346d983f3fd6
zxydaisy <zxy@caicloud.io>
2018-10-24 11:32:53 +0800

    feat: add upload middleware

---
+++ src/components/upload.tsx
@@ -0,0 +1,14 @@
+
+import * as React from 'react';
+
+interface Iprops {
+}
+export default class Uploader extends React.Component<Iprops, {}> {
+  onFileLoad = () => {
+    // TODO
+  }
+
+  render() {
+     <div></div>
+  }
+};
